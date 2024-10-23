80e4edb69f4b1a2eb968db965eedacd5daee25ff
Jamie Cunningham <james.cunningham@udri.udayton.edu>
2024-10-22 22:27:25 -0400

    add sun step test

--- test/sims/inspection/test_sun/test_sun.py
+++ test/sims/inspection/test_sun/test_sun.py
@@ -47,10 +47,24 @@ def test_init_args(name, theta, n, integration_method, material):
             [
                 safe_autonomy_simulation.sims.inspection.Sun.SUN_DISTANCE
                 * np.cos(theta),
-                safe_autonomy_simulation.sims.inspection.Sun.SUN_DISTANCE
+                -safe_autonomy_simulation.sims.inspection.Sun.SUN_DISTANCE
                 * np.sin(theta),
                 0,
             ]
         )
     )
     assert sun.dynamics.n == n
+
+
+@pytest.mark.parametrize(
+    "theta, step_size",
+    [
+        (0, 1),
+        (np.pi, 1),
+        (0, 100),
+    ]
+)
+def test_step(theta, step_size):
+    sun = safe_autonomy_simulation.sims.inspection.Sun(theta=theta)
+    sun.step(step_size=step_size)
+    assert sun.theta - (theta + (sun.n * step_size)) < 0.0000001  # epsilon comparison accounts for rounding error
